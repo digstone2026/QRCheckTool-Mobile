49251d8ad04f2b62a8717d033e886c130c063da6
digstone2026 <digstone@139.com>
2026-06-18 08:32:42 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/qrchecktool/MainActivity.java
+++ app/src/main/java/com/example/qrchecktool/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends Activity {
         tvStatus.setTextColor(Color.WHITE);
 
         tvInfo = new TextView(this);
-        tvInfo.setText("v1.1-jazhao-20260616");
+        tvInfo.setText("v1.2-jazhao-20260618");
         tvInfo.setGravity(Gravity.CENTER);
         tvInfo.setTextColor(Color.WHITE);
 
